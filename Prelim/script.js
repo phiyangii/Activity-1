f0d285ee8e2ab60a450644c7096241c999ec19d8
unknown <ma.copat@lsu.edu.ph>
2022-03-22 23:50:59 -0700

    Prelim

---
+++ Prelim/script.js
@@ -0,0 +1,18 @@
+document.getElementById("Home").onclick = function() {
+  location.href = "#home";
+};
+
+document.getElementById("About").onclick = function() {
+  location.href = "#about";
+};
+
+document.getElementById("Education").onclick = function() {
+  location.href = "#education";
+};
+
+document.getElementById("Contact").onclick = function() {
+  location.href = "#contact";
+};
+document.getElementById("Social Media").onclick = function() {
+  location.href = "#social media";
+};
